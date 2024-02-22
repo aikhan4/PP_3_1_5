b151578f3b07c029a90fb7f7bbfa74fb9ab56e4b
Айхан Касумов <akasum99@gmail.com>
2024-02-22 19:01:02 +0400

    решение(2)

--- src/main/java/com/example/Test_PP_3_1_2/models/User.java
+++ src/main/java/com/example/Test_PP_3_1_2/models/User.java
@@ -2,6 +2,7 @@ package com.example.Test_PP_3_1_2.models;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -45,5 +46,27 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(surname, user.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname);
     }
 }
